e0318ef019043b93dd1c0ae523ffa26fa73cebd2
Olaf-Erkemeij <o.a.erkemeij@gmail.com>
2024-02-21 10:59:05 +0100

    Complete rework of Docker

---
+++ src/resources/js/Pages/Posts/Index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import Post from '@/Components/Post';
+import InputError from '@/Components/InputError';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { useForm, Head } from '@inertiajs/react';
+
+
+export default function Index({ auth, posts }) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        title: '',
+        content: '',
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('posts.store'), { onSuccess: () => reset() });
+    };
+
+    return (
+        <AuthenticatedLayout user={auth.user}>
+            <Head title="Posts" />
+
+            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                <form onSubmit={submit}>
+                    <input
+                        value={data.title}
+                        placeholder="Title"
+                        className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                        onChange={e => setData('title', e.target.value)}
+                    />
+                    <textarea
+                        value={data.content}
+                        placeholder="What do you want to share??"
+                        className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                        onChange={e => setData('content', e.target.value)}
+                    ></textarea>
+                    <InputError message={errors.content} className="mt-2" />
+                    <PrimaryButton className="mt-4" disabled={processing}>Post</PrimaryButton>
+                </form>
+
+                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    {posts.map(post =>
+                        <Post key={post.id} post={post} />
+                    )}
+                    </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
