21a8cc628fcf36303868cf7d660bf4eaadc41f59
Olaf-Erkemeij <o.a.erkemeij@gmail.com>
2024-03-06 11:30:27 +0100

    Testing pipeline on the peer review system

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use willvincent\Rateable\Rateable;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\RatingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
